adf2bb23a50535220a50178ebe76eb51408b355c
oscarpm5 <oscarp-25@hotmail.es>
2022-02-09 13:35:16 +0100

    Compute world ray direction for the camera pixels

--- Aetherius_Volumetric_Clouds_Unity/Assets/RaymarchCamera.cs
+++ Aetherius_Volumetric_Clouds_Unity/Assets/RaymarchCamera.cs
@@ -3,23 +3,98 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(Camera))]
-[ExecuteInEditMode] [ImageEffectAllowedInSceneView]
+[ExecuteInEditMode]
+[ImageEffectAllowedInSceneView]
 public class RaymarchCamera : MonoBehaviour
 {
+    private Camera _cam;
+
     [SerializeField]
     private Shader _shader;
+    private Material _material;
 
-    Material _material;
+    public Material _rayMarchMaterial
+    {
+        get
+        {
+            if (!_material && _shader)
+            {
+                _material = new Material(_shader);
+                _material.hideFlags = HideFlags.HideAndDontSave;
+            }
+            return _material;
+        }
+    }
 
+    public Camera _camera
+    {
+        get //TODO will this work with the editor cam?
+        {
+            if (!_cam)
+            {
+                _cam = GetComponent<Camera>();
+            }
+            return _cam;
+        }
+
+    }
 
 
     private void OnRenderImage(RenderTexture source, RenderTexture destination)
     {
-        if (_material == null)
-            _material = new Material(_shader);
+        if (_rayMarchMaterial == null)
+        {
+            Graphics.Blit(source, destination);
+            return;
+        }
+
+        _rayMarchMaterial.SetMatrix("_CamFrustum", CamFrustrumFromCam(_camera));
+        _rayMarchMaterial.SetMatrix("_CamToWorldMat", _camera.cameraToWorldMatrix);
 
 
-        Graphics.Blit(source, destination, _material);
+        //Create a screen quad
+        RenderTexture.active = destination;
+        GL.PushMatrix();
+        GL.LoadOrtho();
+        _rayMarchMaterial.SetPass(0);
+        GL.Begin(GL.QUADS);
+        //Bottom Left
+        GL.MultiTexCoord2(0, 0.0f, 0.0f);
+        GL.Vertex3(0.0f, 0.0f, 3.0f);
+        //Bottom Right
+        GL.MultiTexCoord2(0, 1.0f, 0.0f);
+        GL.Vertex3(1.0f, 0.0f, 2.0f);
+        //Top Right
+        GL.MultiTexCoord2(0, 1.0f, 1.0f);
+        GL.Vertex3(1.0f, 1.0f, 1.0f);
+        //Top Left
+        GL.MultiTexCoord2(0, 0.0f, 1.0f);
+        GL.Vertex3(0.0f, 1.0f, 0.0f);
+
+        GL.End();
+        GL.PopMatrix();
     }
 
+
+    private Matrix4x4 CamFrustrumFromCam(Camera cam)
+    {
+        Matrix4x4 frustum = Matrix4x4.identity;
+        float foV = Mathf.Tan(cam.fieldOfView * Mathf.Deg2Rad * 0.5f);
+
+        Vector3 goUp = Vector3.up * foV;
+        Vector3 goRight = Vector3.right * foV * cam.aspect;
+
+
+        Vector3 TL = (-Vector3.forward - goRight + goUp); //TOP LEFT CORNER
+        Vector3 TR = (-Vector3.forward + goRight + goUp); //TOP RIGHT CORNER
+        Vector3 BR = (-Vector3.forward + goRight - goUp); //BOTTOM RIGHT CORNER
+        Vector3 BL = (-Vector3.forward - goRight - goUp); //BOTTOM LEFT CORNER
+
+        frustum.SetRow(0, TL);
+        frustum.SetRow(1, TR);
+        frustum.SetRow(2, BR);
+        frustum.SetRow(3, BL);
+
+        return frustum;
+    }
 }
--- Aetherius_Volumetric_Clouds_Unity/Assets/RaymarchShader.shader
+++ Aetherius_Volumetric_Clouds_Unity/Assets/RaymarchShader.shader
@@ -1,52 +1,73 @@
 Shader "Aetherius/RaymarchShader"
 {
-    Properties
-    {
-        _MainTex ("Texture", 2D) = "white" {}
-    }
-    SubShader
-    {
-        // No culling or depth
-        Cull Off ZWrite Off ZTest Always
+	Properties
+	{
+		_MainTex("Texture", 2D) = "white" {}
+	}
+		SubShader
+	{
+		// No culling or depth
+		Cull Off ZWrite Off ZTest Always
 
-        Pass
-        {
-            CGPROGRAM
-            #pragma vertex vert
-            #pragma fragment frag
+		Pass
+		{
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
 
-            #include "UnityCG.cginc"
+			#include "UnityCG.cginc"
 
-            struct appdata
-            {
-                float4 vertex : POSITION;
-                float2 uv : TEXCOORD0;
-            };
+			sampler2D _MainTex;
 
-            struct v2f
-            {
-                float2 uv : TEXCOORD0;
-                float4 vertex : SV_POSITION;
-            };
+			float4x4 _CamFrustum;//Eye Space
+			float4x4 _CamToWorldMat;//Convert camera to world space
 
-            v2f vert (appdata v)
-            {
-                v2f o;
-                o.vertex = UnityObjectToClipPos(v.vertex);
-                o.uv = v.uv;
-                return o;
-            }
+			struct appdata
+			{
+				float4 vertex : POSITION;
+				float2 uv : TEXCOORD0;
+			};
 
-            sampler2D _MainTex;
+			struct v2f
+			{
+				float2 uv : TEXCOORD0;
+				float4 vertex : SV_POSITION;
+				float3 ray : TEXCOORD1;
+			};
 
-            fixed4 frag (v2f i) : SV_Target
-            {
-                fixed4 col = tex2D(_MainTex, i.uv);
-                // just invert the colors
-                col.rgb = 1 - col.rgb;
-                return col;
-            }
-            ENDCG
-        }
-    }
+			v2f vert(appdata v)
+			{
+				v2f o;
+
+				half index = v.vertex.z; //we use the z component of the vertex as an index for the _CamFrustum matrix
+				v.vertex.z = 0;
+
+				o.vertex = UnityObjectToClipPos(v.vertex);
+				o.uv = v.uv;
+
+				o.ray = _CamFrustum[(int)index].xyz;
+
+				o.ray /= abs(o.ray.z);//Normalize in z direction
+				o.ray = mul(_CamToWorldMat, o.ray);
+
+				return o;
+			}
+
+
+
+			fixed4 frag(v2f i) : SV_Target
+			{
+
+				fixed4 col = tex2D(_MainTex, i.uv);
+			// just invert the colors
+			col.rgb = 1 - col.rgb;
+
+			float3 rayDirection = normalize(i.ray.xyz);
+			float3 rayOrigin = _WorldSpaceCameraPos;
+			col.rgb = rayDirection;
+			return col;
+			}
+		ENDCG
+		}
+	}
 }
