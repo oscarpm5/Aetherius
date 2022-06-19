4f10e2289e4363d491226ea4d8977a2a71eca24c
oscarpm5 <oscarp-25@hotmail.es>
2022-06-19 16:31:48 +0200

    Limit FPS script changed

--- Aetherius_Volumetric_Clouds_Unity/Assets/Aetherius/Scripts/LimitFPS.cs
+++ Aetherius_Volumetric_Clouds_Unity/Assets/Aetherius/Scripts/LimitFPS.cs
@@ -5,16 +5,38 @@ using UnityEngine;
 [ExecuteInEditMode]
 public class LimitFPS : MonoBehaviour
 {
-    [Range(1,120)]
-    public int targetFPS =30;
-    // Update is called once per frame
+#if UNITY_EDITOR
+    [Range(1, 120)]
+    public int targetFPS = 30;
+    int _targetFPS;
+
+
+    private void OnEnable()
+    {
+        QualitySettings.vSyncCount = 0;  // VSync must be disabled
+        Application.targetFrameRate = targetFPS;
+        _targetFPS = targetFPS;
+    }
+
     void Update()
     {
+        // Update is called once per frame
+        if(_targetFPS!=targetFPS)
+        {
+            _targetFPS = targetFPS;
 
-#if UNITY_EDITOR
             QualitySettings.vSyncCount = 0;  // VSync must be disabled
-            Application.targetFrameRate = targetFPS;
-#endif
-        
+            Application.targetFrameRate = _targetFPS;
+
+        }
+
     }
+
+    private void OnDisable()
+    {
+        Application.targetFrameRate = -1;
+
+    }
+
+#endif
 }
