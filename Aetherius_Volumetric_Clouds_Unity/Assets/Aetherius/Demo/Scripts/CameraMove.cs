3a55d6da145183b1ef37f9212c05ed08620fb526
oscarpm5 <oscarp-25@hotmail.es>
2022-06-25 22:15:41 +0200

    Camera Demo updated

--- Aetherius_Volumetric_Clouds_Unity/Assets/Aetherius/Demo/Scripts/CameraMove.cs
+++ Aetherius_Volumetric_Clouds_Unity/Assets/Aetherius/Demo/Scripts/CameraMove.cs
@@ -2,153 +2,164 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
-public class CameraMove : MonoBehaviour
+namespace Aetherius.Demo
 {
-    float minAccel = 0.01f;
-    float maxAccel = 2.0f;
-    float accel = 0.2f;
 
-    float defaultSpeed = 10.0f;
-    public float speed = 0.0f;
-    float rotSpeed = 2.5f;
-    float sprintMultiplier = 3.0f;
-    Vector3 movement = Vector3.zero;
-    float pitch;
-    float yaw;
-
-    float newPitch;
-    float newYaw;
-
-    [HideInInspector]
-    public bool enabledControl = true;
-    bool fpCamMode = true;//first person cam mode
-
-
-    Vector3 camOrigin = new Vector3(0.0f, 2.0f, 0.0f);
-
-    
-    
-    // Start is called before the first frame update
-    void Start()
+    public class CameraMove : MonoBehaviour
     {
-        speed = defaultSpeed;
-        SetPitchYawPos(pitch, yaw, camOrigin);
-    }
+        float minAccel = 0.01f;
+        float maxAccel = 2.0f;
+        float accel = 1.0f;
 
-    // Update is called once per frame
-    void Update()
-    {
+        float defaultSpeed = 10.0f;
+        public float speed = 0.0f;
+        float rotSpeed = 2.5f;
+        float sprintMultiplier = 3.0f;
+        Vector3 movementDir = Vector3.zero;
+        Vector3 lastInputDir = Vector3.zero;
+        float pitch;
+        float yaw;
 
-        HandleInput();
+        float newPitch;
+        float newYaw;
 
-        Cursor.lockState = fpCamMode && enabledControl ? CursorLockMode.Locked : CursorLockMode.None;
-
-    }
-
-
-    void HandleInput()
-    {
-        if (!enabledControl)
-            return;
-
-        //UI Mode
-        fpCamMode = Input.GetMouseButton(1);
-
-        newYaw = 0.0f;
-        newPitch = 0.0f;
-        Vector3 newMovement = Vector3.zero;
-
-        bool isInputingMovement = false;
+        [HideInInspector]
+        public bool enabledControl = true;
+        bool fpCamMode = true;//first person cam mode
 
 
-        if (Input.GetKey(KeyCode.F))
+        Vector3 camOrigin = new Vector3(0.0f, 2.0f, 0.0f);
+
+
+
+        // Start is called before the first frame update
+        void Start()
         {
             speed = 0;
-            gameObject.transform.position = camOrigin;
+            SetPitchYawPos(pitch, yaw, camOrigin);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+            HandleInput();
+
+            Cursor.lockState = fpCamMode && enabledControl ? CursorLockMode.Locked : CursorLockMode.None;
+
         }
 
 
-        if (fpCamMode)
+        void HandleInput()
         {
-            //Rotation
-            newYaw = Input.GetAxis("Mouse X");//Pitch
-            newPitch = Input.GetAxis("Mouse Y");//Yaw
+            if (!enabledControl)
+                return;
+
+            //UI Mode
+            fpCamMode = Input.GetMouseButton(1);
+
+            newYaw = 0.0f;
+            newPitch = 0.0f;
+
+            bool isInputingMovement = false;
 
 
-            float wheelAxis = Input.GetAxis("Mouse ScrollWheel");
-            if (wheelAxis < 0.0f)
+            if (Input.GetKey(KeyCode.F))
             {
-                accel -= accel * Mathf.Abs(wheelAxis) * 1.5f;
+                speed = 0;
+                gameObject.transform.position = camOrigin;
+                movementDir = Vector3.zero;
             }
-            else if (wheelAxis > 0.0f)
+
+
+            if (fpCamMode)
             {
-                accel += accel * Mathf.Abs(wheelAxis) * 1.5f;
+                //Rotation
+                newYaw = Input.GetAxis("Mouse X");//Pitch
+                newPitch = Input.GetAxis("Mouse Y");//Yaw
+
+
+                float wheelAxis = Input.GetAxis("Mouse ScrollWheel");
+                if (wheelAxis < 0.0f)
+                {
+                    accel -= accel * Mathf.Abs(wheelAxis) * 1.5f;
+                }
+                else if (wheelAxis > 0.0f)
+                {
+                    accel += accel * Mathf.Abs(wheelAxis) * 1.5f;
+                }
+
+                accel = Mathf.Clamp(accel, minAccel, maxAccel);
+
+
+                pitch -= newPitch * rotSpeed;
+                yaw += newYaw * rotSpeed;
+
+                gameObject.transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
+
+
+
+
+                //Movement
+                float up = 0.0f;
+                if (Input.GetKey(KeyCode.Q))
+                    up += -1.0f;
+                if (Input.GetKey(KeyCode.E))
+                    up += 1.0f;
+
+                Vector3 newInputDir = new Vector3(Input.GetAxis("Horizontal"), up, Input.GetAxis("Vertical"));
+
+                if (newInputDir.sqrMagnitude > 0.0f)
+                {
+                    isInputingMovement = true;
+                    lastInputDir = newInputDir.normalized;
+
+                    speed += 0.2f*speed * accel * Time.deltaTime * (Input.GetKey(KeyCode.LeftShift) ? sprintMultiplier : 1.0f);
+                    speed = Mathf.Max(speed, defaultSpeed);          
+                   
+                }
+
             }
 
-            accel = Mathf.Clamp(accel, minAccel, maxAccel);
 
+            movementDir = Vector3.Lerp(movementDir, lastInputDir,5.0f* Time.deltaTime);
 
-            pitch -= newPitch * rotSpeed;
-            yaw += newYaw * rotSpeed;
+            if (!isInputingMovement)
+            {
+
+                speed -= speed* Mathf.Clamp01(20.0f*Time.deltaTime);
+
+                if (speed < defaultSpeed)
+                {
+                    movementDir = Vector3.zero;
+                    lastInputDir = Vector3.zero;
+                    speed = 0.0f;
+                }
+            }
+
+           
+
+            gameObject.transform.position += gameObject.transform.rotation * movementDir.normalized * speed * Time.deltaTime;
+
+        }
+
+        public void SetPitchYaw(float pitch, float yaw)
+        {
+            this.pitch = pitch;
+            this.yaw = yaw;
 
             gameObject.transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
-
-
-
-
-            //Movement
-            float up = 0.0f;
-            if (Input.GetKey(KeyCode.Q))
-                up += -1.0f;
-            if (Input.GetKey(KeyCode.E))
-                up += 1.0f;
-
-            newMovement = new Vector3(Input.GetAxis("Horizontal"), up, Input.GetAxis("Vertical"));
-
-            if (newMovement.sqrMagnitude > 0.0f)
-            {
-                isInputingMovement = true;
-
-                newMovement.Normalize();
-
-                movement = Vector3.Lerp(movement, newMovement, 5.0f * Time.deltaTime);
-                movement.Normalize();
-                speed = Mathf.Max(speed, defaultSpeed);
-
-                speed += speed * accel * Time.deltaTime * (Input.GetKey(KeyCode.LeftShift) ? sprintMultiplier : 1.0f) + Time.deltaTime;
-            }
-
         }
 
-
-        if (!isInputingMovement)
+        public void SetPitchYawPos(float pitch, float yaw, Vector3 pos)
         {
-            speed = speed - speed * Mathf.Clamp01(10.0f * Time.deltaTime);
-            if (speed < defaultSpeed)
-                speed = 0;
+            SetPitchYaw(pitch, yaw);
+
+            movementDir = Vector3.zero;
+            speed = defaultSpeed;
+            gameObject.transform.position = pos;
         }
 
-
-        gameObject.transform.position += gameObject.transform.rotation * movement * speed * Time.deltaTime;
-
-    }
-
-    public void SetPitchYaw(float pitch, float yaw)
-    {
-        this.pitch = pitch;
-        this.yaw = yaw;
-
-        gameObject.transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
-    }
-
-    public void SetPitchYawPos(float pitch, float yaw, Vector3 pos)
-    {
-        SetPitchYaw(pitch, yaw);
-
-        movement = Vector3.zero;
-        speed = defaultSpeed;
-        gameObject.transform.position = pos;
     }
 
 }
--- Aetherius_Volumetric_Clouds_Unity/Assets/Aetherius/Scripts/LimitFPS.cs
+++ Aetherius_Volumetric_Clouds_Unity/Assets/Aetherius/Scripts/LimitFPS.cs
@@ -2,49 +2,53 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-//Helper Script, limits FPS on the editor window as to not overload the GPU while developing the game
-[ExecuteInEditMode]
-public class LimitFPS : MonoBehaviour
+namespace Aetherius
 {
+    //Helper Script, limits FPS on the editor window as to not overload the GPU while developing the game
+    [ExecuteInEditMode]
+    public class LimitFPS : MonoBehaviour
+    {
 #if UNITY_EDITOR
-    [Range(1, 120)]
-    public int targetFPS = 30;
-    int _targetFPS;
+        [Range(1, 120)]
+        public int targetFPS = 30;
+        int _targetFPS;
 
 
-    private void OnEnable()
-    {
-        if (!Application.isPlaying)
+        private void OnEnable()
         {
-            QualitySettings.vSyncCount = 0;  // VSync must be disabled
-            Application.targetFrameRate = targetFPS;
-            _targetFPS = targetFPS;
-        }
-        else
-        {
-            this.enabled = false;
-        }
-    }
-
-    void Update()
-    {
-        // Update is called once per frame
-        if (_targetFPS != targetFPS)
-        {
-            _targetFPS = targetFPS;
-
-            QualitySettings.vSyncCount = 0;  // VSync must be disabled
-            Application.targetFrameRate = _targetFPS;
-
+            if (!Application.isPlaying)
+            {
+                QualitySettings.vSyncCount = 0;  // VSync must be disabled
+                Application.targetFrameRate = targetFPS;
+                _targetFPS = targetFPS;
+            }
+            else
+            {
+                this.enabled = false;
+            }
         }
 
-    }
+        void Update()
+        {
+            // Update is called once per frame
+            if (_targetFPS != targetFPS)
+            {
+                _targetFPS = targetFPS;
 
-    private void OnDisable()
-    {
-        Application.targetFrameRate = -1;
+                QualitySettings.vSyncCount = 0;  // VSync must be disabled
+                Application.targetFrameRate = _targetFPS;
 
-    }
+            }
+
+        }
+
+        private void OnDisable()
+        {
+            Application.targetFrameRate = -1;
+
+        }
 
 #endif
+    }
+
 }
